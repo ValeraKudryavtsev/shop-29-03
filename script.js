aaa0ba543ad1157b4049428d460382cce68bb3e5
valera_kudryavtsev <shrhannor@gmail.com>
2022-04-17 20:02:19 +0300

    fix some bags, made adaptive

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let button = document.getElementById('button');
 
 let navigation = document.getElementById('menu');
 
+let closeButton = document.getElementById('close-menu');
+
 button.addEventListener('click', function() {
-    navigation.classList.toggle('display-block');
+    navigation.classList.toggle('header__display-block');
 });
